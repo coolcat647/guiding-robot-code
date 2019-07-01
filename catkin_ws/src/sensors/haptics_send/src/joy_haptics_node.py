c7e9a8f61a5073614fc7de1b546cd7901e804aa8
coolcat647 <b10302017@mail.ntust.edu.tw>
2019-07-02 00:21:23 +0800

    combine bt whistband and bt headset

--- catkin_ws/src/sensors/haptics_send/src/joy_haptics_node.py
+++ catkin_ws/src/sensors/haptics_send/src/joy_haptics_node.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import rospy
 import sys
+import math
 from BluetoothService import *
 from bitstring import BitArray
 from haptics_msgs.msg import VibrationArray
@@ -22,21 +23,32 @@ class JoyHaptics(object):
         self.vb_msg.frequencies = [0, 0, 0]
         self.vb_msg.intensities = [0, 0, 0]
 
-        if msg.axes[0] == 0:
-            self.vb_msg.frequencies[1] = 3
-            self.vb_msg.intensities[1] = 3
-        elif msg.axes[0] > 0:                   # left
-            self.vb_msg.frequencies[0] = int(abs(msg.axes[0]) * 3)
-            self.vb_msg.intensities[0] = int(abs(msg.axes[0]) * 4)
-        else:                                   # right
-            self.vb_msg.frequencies[2] = int(abs(msg.axes[0]) * 3)
-            self.vb_msg.intensities[2] = int(abs(msg.axes[0]) * 4)
+        if msg.axes[1] > 0:                     # front
+            self.vb_msg.frequencies[1], self.vb_msg.intensities[1] = self.vb_pattern_select(round(abs(msg.axes[1]) * 2))
+
+        if msg.axes[0] > 0:                     # left
+            self.vb_msg.frequencies[0], self.vb_msg.intensities[0] = self.vb_pattern_select(round(abs(msg.axes[0]) * 2))
+        else:                                   # right 
+            self.vb_msg.frequencies[2], self.vb_msg.intensities[2] = self.vb_pattern_select(round(abs(msg.axes[0]) * 2))    
 
         if msg.buttons[0] == 1:
             self.pub_vib.publish(self.vb_msg)
+
+    def vb_pattern_select(self, level=0):
+        freq = 0
+        intensity = 0
+
+        if level == 0:
+            freq, intensity = 0, 0
+        elif level == 1:
+            freq, intensity = 1, 2
+        elif level == 2:
+            freq, intensity = 3, 4
+        
+        return freq, intensity
     
 if __name__ == '__main__':
-    rospy.init_node("haptic_send",anonymous=False)
+    rospy.init_node("joy_haptics_node", anonymous=False)
     node = JoyHaptics()
     rospy.spin()
 
--- catkin_ws/src/sensors/sound_test/src/sound_play.py
+++ catkin_ws/src/sensors/sound_test/src/sound_play.py
@@ -5,24 +5,44 @@ import numpy as np
 import time
 import rospy
 import rospkg
+from haptics_msgs.msg import VibrationArray
 
-rospy.init_node('sound_test_node', anonymous=False)
-rospack = rospkg.RosPack()
-pkg_path = rospack.get_path('sound_test')
-print(pkg_path)
+RIGHT_ALARM_FILE = 'right_obs.mp3'
+LEFT_ALARM_FILE = 'left_obs.mp3'
+FRONT_ALARM_FILE = 'front_obs.mp3'
 
-sound_right = AudioSegment.from_mp3(pkg_path + "/src/right_obs.mp3")
-stereo_right = sound_right.apply_gain_stereo(-20, +6)
+class BTSound(object):
+    def __init__(self):
+        
+        rospack = rospkg.RosPack()
+        self.sound_folder_path = rospack.get_path('sound_test') + '/src/'
 
-sound_left = AudioSegment.from_mp3(pkg_path + "/src/left_obs.mp3")
-stereo_left = sound_left.apply_gain_stereo(+6, -20)
+        # Audio source init
+        sound_right = AudioSegment.from_mp3(sound_folder_path + RIGHT_ALARM_FILE)
+        self.stereo_right = sound_right.apply_gain_stereo(-20, +6)   # simulate right channel only sound
+        sound_left = AudioSegment.from_mp3(sound_folder_path + LEFT_ALARM_FILE)
+        self.stereo_left = sound_left.apply_gain_stereo(+6, -20)     # simulate left channel only sound
+        self.stereo_front = AudioSegment.from_mp3(sound_folder_path + FRONT_ALARM_FILE)
 
-sound_front = AudioSegment.from_mp3(pkg_path + "/src/front_obs.mp3")
-# stereo_left = sound_left.apply_gain_stereo(+6, -20)
+        # Subscriber
+        sub_vibration = rospy.Subscriber("vibrate_cmd", VibrationArray, self.vibration_cb)
 
-play(stereo_right)
+    def vibration_cb(self, msg):
+        if len(msg.frequencies) != len(msg.intensities)
+            rospy.signal_shutdown('The length of freq array and the length of intensity are not equal')
+        
+        if msg.frequencies[1] > 3:
+            play(stereo_front)
+        elif msg.frequencies[0] > 3:
+            play(stereo_left)
+        elif msg.frequencies[2] > 3:
+            play(stereo_right)
 
-# time.sleep(1)
+    def shutdown_cb(self):
+        rospy.loginfo("[%s] Shutting down..." % rospy.get_name())
 
-play(stereo_left)
-play(sound_front)
+if __name__ == '__main__':
+    rospy.init_node('bt_sound_node', anonymous=False)
+    node = BTSound()
+    rospy.on_shutdown(node.shutdown_cb)
+    rospy.spin()
