bed6ac0e51066863188ffe2799b1b56390f908f1
coolcat647 <b10302017@mail.ntust.edu.tw>
2019-07-02 01:02:59 +0800

    fix headset bug

--- catkin_ws/src/sensors/sound_test/src/sound_play.py
+++ catkin_ws/src/sensors/sound_test/src/sound_play.py
@@ -18,25 +18,31 @@ class BTSound(object):
         self.sound_folder_path = rospack.get_path('sound_test') + '/src/'
 
         # Audio source init
-        sound_right = AudioSegment.from_mp3(sound_folder_path + RIGHT_ALARM_FILE)
+        sound_right = AudioSegment.from_mp3(self.sound_folder_path + RIGHT_ALARM_FILE)
         self.stereo_right = sound_right.apply_gain_stereo(-20, +6)   # simulate right channel only sound
-        sound_left = AudioSegment.from_mp3(sound_folder_path + LEFT_ALARM_FILE)
+        sound_left = AudioSegment.from_mp3(self.sound_folder_path + LEFT_ALARM_FILE)
         self.stereo_left = sound_left.apply_gain_stereo(+6, -20)     # simulate left channel only sound
-        self.stereo_front = AudioSegment.from_mp3(sound_folder_path + FRONT_ALARM_FILE)
+        self.stereo_front = AudioSegment.from_mp3(self.sound_folder_path + FRONT_ALARM_FILE)
 
         # Subscriber
         sub_vibration = rospy.Subscriber("vibrate_cmd", VibrationArray, self.vibration_cb)
 
+        self.prev_alarm = -1
+        # play(self.stereo_right)
+
     def vibration_cb(self, msg):
-        if len(msg.frequencies) != len(msg.intensities)
+        if len(msg.frequencies) != len(msg.intensities):
             rospy.signal_shutdown('The length of freq array and the length of intensity are not equal')
         
-        if msg.frequencies[1] > 3:
-            play(stereo_front)
-        elif msg.frequencies[0] > 3:
-            play(stereo_left)
-        elif msg.frequencies[2] > 3:
-            play(stereo_right)
+        if msg.frequencies[1] >= 3 and self.prev_alarm != 1:
+            play(self.stereo_front)
+            self.prev_alarm = 1
+        elif msg.frequencies[0] >= 3 and self.prev_alarm != 0:
+            play(self.stereo_left)
+            self.prev_alarm = 0
+        elif msg.frequencies[2] >= 3 and self.prev_alarm != 2:
+            play(self.stereo_right)
+            self.prev_alarm = 2
 
     def shutdown_cb(self):
         rospy.loginfo("[%s] Shutting down..." % rospy.get_name())
